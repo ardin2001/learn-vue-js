f5472f12b1ab8ff37bbc1d6970aaf12544051327
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-09-07 10:13:10 +0700

    learn-1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <p>Header Vue-1</p>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <h3>Home</h3>
+    <form action="" id="form">
+      <div class="name">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" v-model="name" />
+      </div>
+
+      <div class="email">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" v-model="email" />
+      </div>
+
+      <div class="password">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" v-model="password" />
+      </div>
+      <div class="result">
+        <p>Name: {{ name }}</p>
+        <p>Email: {{ email }}</p>
+        <p>Password: {{ password }}</p>
+      </div>
+    </form>
+  </main>
+</template>
+
+<script setup>
+import { ref, createApp } from 'vue'
+
+const name = ref('')
+const email = ref('')
+const password = ref('')
+
+// const app = createApp({
+//   setup() {
+//     return {
+//       name,
+//       email,
+//       password
+//     }
+//   }
+// })
+
+// app.mount('#form')
+</script>
