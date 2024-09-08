aee1b5338618ea4a73d84b1ea6d2fa22545fa4ce
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-09-08 10:41:53 +0700

    conditional rendering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,26 +16,47 @@
         <label for="password">Password</label>
         <input type="password" name="password" id="password" v-model="password" />
       </div>
+      
+      <div class="jk">
+        <label for="jk">Jenis Kelamin :</label>
+          <input type="radio" value="pria" name="jk" id="pria" v-model="jk" />
+          <label for="pria">Pria</label>
+          <input type="radio" value="wanita" name="jk" id="wanita" v-model="jk" />
+          <label for="wanita">Wanita</label>
+      </div>
+
       <div class="result">
         <p>Name: {{ name }}</p>
         <p>Email: {{ email }}</p>
-        <p>Password: {{ password }}</p>
+        <p>Password: {{ password.length>6 ? password : password.length==0 ? ''  : 'password kurang dari 6' }}</p>
+        <p v-if="jk=='pria'">Jenis kelamin Laki-laki</p>
+        <p v-else-if="jk=='wanita'">Jenis kelamin Perempuan</p>
+        <p v-else>Jenis kelamin Belum dipilih</p>
       </div>
+      <br>
+      <button @click="ResetInput" type="button">Reset</button>
     </form>
   </main>
 </template>
 
-<!-- <script setup>
-import { ref  } from 'vue'
+<script setup>
+import { ref } from 'vue'
 
 const name = ref('ardin')
 const email = ref('')
 const password = ref('')
-</script> -->
+const jk = ref('')
+function ResetInput() {
+  name.value = ''
+  email.value = ''
+  password.value = ''
+  jk.value = ''
+}
+</script>
 
 <!-- perbedaan ketika tanpa menggunakan script setup -->
 <!-- membutuhkan export default kemudian di setup dengan return valuenya -->
-<script>
+<!-- <script>
 import { ref } from 'vue'
 
 export default {
@@ -48,4 +69,4 @@ export default {
     }
   }
 }
-</script>
+</script> -->
